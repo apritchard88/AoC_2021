1d02216e3118aeee6bd053d64cd87555fbe37df5
Adrian Pritchard <adrian_pritchard@hotmail.co.uk>
2021-12-23 17:00:48 +0000

    add: day 21 pt2

--- Day21_solution.R
+++ Day21_solution.R
@@ -77,3 +77,75 @@ print(answer_pt1)
 
 ### part 2
 # don't know where to start with this...
+# had a little look at the reddit thread for some ideas
+# led to recursion and memoisation
+target <- 21
+results <- c()
+names(results) <- c()
+# memory will become a list of all possible states that we encounter, along with the number of wins from that state
+# the idea is that it serves as a cache of the states and outcomes we have already encountered
+# therefore if we end up back at that state we don't need to recalculate - just use the previous answer
+# this cache idea is called memoisation (not actually sure I did it right, but it works well)
+memory <- list()
+next_turn <- function(p1_pos, p2_pos, p1_score, p2_score, turn){
+  # each state is uniquely identified by this name
+  this_state <- paste(p1_pos, p2_pos, p1_score, p2_score, turn, sep = ",")
+  # if there is a winner, return 1 or 0 depending on which player we are counting for
+  # need to run twice - change which player we are counting the wins of here
+  if(p1_score >= target){
+    win <- 1
+    return(win)
+  }
+  if(p2_score >= target){
+    win <- 0
+    return(win)
+  }
+  key <- this_state
+  # this is the point at which we look if we were in this state before - reuse results if so
+  if(key %in% names(memory)){
+    return(memory[[key]])
+  }
+  # otherwise we are in an unvisited state and need to calculate wins from scratch
+  results <- 0
+  # roll the dice 3 times and find the sum
+  # need to do it for all combinations
+  for(d1 in c(1,2,3)){
+    for(d2 in c(1,2,3)){
+      for(d3 in c(1,2,3)){
+        this_roll <- sum(d1,d2,d3)
+        # if it is player 1's turn
+        if(turn == 1){
+          new_p1_pos <- (p1_pos + this_roll)%%10 # new position after adding the roll
+          if(new_p1_pos == 0){new_p1_pos <- 10} # if we have 0, actual space is 10
+          new_p1_score <- p1_score + new_p1_pos # calculate new score
+          # now we use recursion - call the function from inside the function
+          # we call this function with the updates game state
+          # this will happen repeatedly until winners are returned
+          result <- next_turn(new_p1_pos, p2_pos, new_p1_score, p2_score, 2)
+          # after we grab the results, add them up for this state
+          results <- results + result
+        # repeat the scenario but for player 2's turns
+        }else{
+          new_p2_pos <- (p2_pos + this_roll)%%10
+          if(new_p2_pos == 0){new_p2_pos <- 10}
+          new_p2_score <- p2_score + new_p2_pos
+          result <- next_turn(p1_pos, new_p2_pos, p1_score, new_p2_score, 1)
+          results <- results + result
+        }
+        
+      }
+    }
+  }
+  # now we fill in the cache with the results from this game state
+  # use global assignment <<- as the list is outside the function and must be modified each time
+  memory[[key]] <<- results
+  
+  # return the results from this state
+  # ultimate return to console will be for the input state
+  return(memory[[key]])
+}
+# just call the function with the desired state, answer will print to screen
+next_turn(start_p1, start_p2, 0, 0, 1)
+
+# player 1 wins = 919758187195363
+# player 2 wins = 635572886949720
