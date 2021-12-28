877dc7a47adccedb0d518dd66290db379ca2dacc
Adrian Pritchard <adrian_pritchard@hotmail.co.uk>
2021-12-28 13:36:49 +0000

    mod: day 12 part 2 solution

--- Day12_solution.R
+++ Day12_solution.R
@@ -26,7 +26,6 @@ create_new_upper_df <- function(df, val, rep){
   return(df_new)
 }
 
-#u<-create_new_upper_df(day12_data, 'RW')
 new_data <- day12_data
 added_vertices <- c()
 for(vertex in all_vertices){
@@ -85,3 +84,45 @@ answer_part1 <- length(actual_paths)
 # 5) loop through paths and change eg HN2 back to derivative
 # 6) collapse path to a string and output
 # 7) change the output to a data frame and use distinct() to find all distinct paths
+
+# part 2 needs a different approach
+# try to record all paths recursively?
+
+# for part 2 we have the option to visit one small cave twice
+# this function is recursive, finds next node and counts all those that end in 'end'
+count_paths <- function(start_node, visited, doubled){
+  # if the node is 'end' we found a path - return 1 for the count
+  if(start_node == "end"){
+    return(1)
+  }
+  path_count <- 0 # set the path count to 0 to start for the next loop
+  next_nodes <- neighbors(g,start_node) # get a list of adjoining nodes from the graph
+  # now loop through the next nodes
+  for(i in 1:length(next_nodes)){
+    next_node <- next_nodes[i]$name
+    # if the next node is 'start' we cannot visit it
+    if(next_node == 'start'){
+      next
+    }else if((next_node %in% visited) & doubled){
+      # if the next node is in the lower case visited list and we already went to one twice, we cannot visit it
+      next
+    }else{
+      # track the lower case nodes we have already visited
+      if(start_node %in% lower_list){
+        visited <- append(visited, start_node)
+      }
+      # if the next node is lower case and has been visited, change 'doubled' bool to TRUE for rest of path
+      # this stops the path visiting any more small caves a second time
+      if((next_node %in% visited)){
+        path_count <- path_count + count_paths(next_node, visited, TRUE)
+      }else{
+        # otherwise we keep the doubled variable as it is
+        path_count <- path_count + count_paths(next_node, visited, doubled)
+      }
+    }
+  }
+  return(path_count)
+}
+
+visited_nodes <- c()
+answer_pt2 <- count_paths('start',visited_nodes, FALSE)
